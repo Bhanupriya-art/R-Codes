b4c77e2df8cbda14d5873695e1c2f23939c28592
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-02-13 09:53:52 +0530

    Add files via upload

--- Advance_data_Types_Arrays_Lec_9.R
+++ Advance_data_Types_Arrays_Lec_9.R
@@ -43,12 +43,24 @@ name2 <- c("col1","col2","col3","col4","col5")
 array2 <- array(c(vector3,vector4), dim= c(4,5), dimnames = list(name1,name2))
 array2
 
+# create 2 vectors. first vector length is 3 and the elements are 3,4,5 and name it as v1.
+# 2nd vector of length 8 having the values 10,20,30,40,50,60,70,80 and name it as v2.
+# Create an arrays forming this v1 and v2 together the dimension is 3 X 3.
+#The names of the rows must be score 1 , 2 and 3 and columns are course 1,2,3. and name of the arrays as
+# Create 4 arrays and name it as array1 and so on.. 
 
 
+v1 <- c(1,2,3)
+v2 <- c(10,20,30,40,50,60,70,80)
+name1 <- c("Score1","Score2","Score3")
+name2 <- c("Course1","Course2","Course3")
+name3 <- c("Array1","Array2","Array3","Array4")
 
+arrays <- array(c(v1,v2), dim = c(3,3,4), dimnames = list(name1,name2,name3))
+arrays
 
-
-
-
+# Access the rows and columns through names
+arrays[,"Course2",] #columns -> This is course2 of every array 
+arrays["Score1",,] #rows -> This is Score1 of every array 
 
 
